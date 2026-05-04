defb15ab43d63219e239830db2e3e116a63aea85
mmueller <mmueller@prisma-software.de>
2026-05-04 14:27:24 +0200

    init

--- app.py
+++ app.py
@@ -29,12 +29,17 @@ def save_layout():
     #201 = HTTP-Statuscode -> Created
     return jsonify({"status": "success", "message": "Layout gespeichert"}), 201
 
-@app.route('/api/layout/<id>', methods=['GET'])
-def get_layout_id(id):
+@app.route('/api/layout', methods=['GET'])
+def get_layout_id():
     #TODO: Layout mit der entsprechenden <id> aus MS-SQL laden
+    layout_id = request.args.get("id")
 
+
+    if not layout_id:
+        #400 = HTTP-Statuscode -> Bad Request
+        return jsonify({"error": "Keine ID angegeben"}), 400
     #200 = HTTP-Statuscode -> OK
-    return jsonify({"status": "Ddebug", "message": f"Wprde Layout {id} laden"}), 200
+    return jsonify({"status": "debug", "message": f"Würde Layout #{layout_id} laden"}), 200
 
 @app.route('/api/layouts', methods=['GET'])
 def get_layouts():
